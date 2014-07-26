1ab6462f7095d4a151fde5f3bc0e7aac7fa617dc
Jon <agmlauncher@gmail.com>
2014-07-25 21:41:58 -0400

    adding div wrapper styling

--- src/views/queries.blade.php
+++ src/views/queries.blade.php
@@ -1,4 +1,6 @@
-<p>Total Queries: {{ $QueryLogPresenter->getQueryCount() }}</p>
-@foreach($QueryLogPresenter->getQueries() as $query)
-	<p>{{ $query['time'] }} : {{ $query['query'] }}</p>
-@endforeach
+<div id="jlem-profiler" style="margin-top: 50px; border-top: 1px solid #e2e2e2; background: #f4f4f4; padding: 50px;">
+	<p>Total Queries: {{ $QueryLogPresenter->getQueryCount() }}</p>
+	@foreach($QueryLogPresenter->getQueries() as $query)
+		<p>{{ $query['time'] }} : {{ $query['query'] }}</p>
+	@endforeach
+</div>
